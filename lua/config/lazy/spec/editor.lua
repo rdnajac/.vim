5b165b40b5f52dbc600609fb1ed4274aeb3d4616
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-11 12:57:03 -0400

    add folke plugins

--- lua/config/lazy/spec/editor.lua
+++ lua/config/lazy/spec/editor.lua
@@ -35,6 +35,8 @@ return {
               return require('which-key.extras').expand.win()
             end,
           },
+
+          { '<leader>x', group = 'diagnostics/quickfix', icon = { icon = '󱖫 ', color = 'green' } },
           {
             icon = { icon = ' ', color = 'green' },
             { '<leader>r' },
@@ -52,6 +54,67 @@ return {
           { 'g*' },
         },
       },
+    },
+  },
+  {
+    'folke/trouble.nvim',
+    cmd = { 'Trouble' },
+    opts = {
+      modes = {
+        lsp = {
+          win = { position = 'right' },
+        },
+      },
+    },
+    keys = {
+      { '<leader>xx', '<Cmd>Trouble diagnostics toggle<CR>', desc = 'Diagnostics (Trouble)' },
+      { '<leader>xX', '<Cmd>Trouble diagnostics toggle filter.buf=0<CR>', desc = 'Buffer Diagnostics (Trouble)' },
+      { '<leader>cs', '<Cmd>Trouble symbols toggle<CR>', desc = 'Symbols (Trouble)' },
+      { '<leader>cS', '<Cmd>Trouble lsp toggle<CR>', desc = 'LSP references/definitions/... (Trouble)' },
+      { '<leader>xL', '<Cmd>Trouble loclist toggle<CR>', desc = 'Location List (Trouble)' },
+      { '<leader>xQ', '<Cmd>Trouble qflist toggle<CR>', desc = 'Quickfix List (Trouble)' },
+      {
+        '[q',
+        function()
+          if require('trouble').is_open() then
+            require('trouble').prev({ skip_groups = true, jump = true })
+          else
+            local ok, err = pcall(vim.cmd.cprev)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
+          end
+        end,
+        desc = 'Previous Trouble/Quickfix Item',
+      },
+      {
+        ']q',
+        function()
+          if require('trouble').is_open() then
+            require('trouble').next({ skip_groups = true, jump = true })
+          else
+            local ok, err = pcall(vim.cmd.cnext)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
+          end
+        end,
+        desc = 'Next Trouble/Quickfix Item',
+      },
+    },
+  },
+  {
+    'folke/todo-comments.nvim',
+    event = 'LazyFile',
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { ']t', function() require('todo-comments').jump_next() end, desc = 'Next Todo Comment', },
+      { '[t', function() require('todo-comments').jump_prev() end, desc = 'Previous Todo Comment', },
+      { '<leader>xt', '<Cmd>Trouble todo toggle<CR>', desc = 'Todo (Trouble)' },
+      { '<leader>xT', '<Cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<CR>', desc = 'Todo/Fix/Fixme (Trouble)', },
+      { '<leader>st', '<Cmd>TodoTelescope<CR>', desc = 'Todo' },
+      { '<leader>sT', '<Cmd>TodoTelescope keywords=TODO,FIX,FIXME<CR>', desc = 'Todo/Fix/Fixme' },
     },
   },
 }
