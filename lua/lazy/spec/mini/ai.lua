bb66451639ea5af8b18699c0789931f19c581df4
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-26 14:13:55 -0400

    use mini ai

--- lua/lazy/spec/mini/ai.lua
+++ lua/lazy/spec/mini/ai.lua
@@ -1,15 +1,32 @@
+-- taken from MiniExtra.gen_ai_spec.buffer
+local function ai_buffer(ai_type)
+  local start_line, end_line = 1, vim.fn.line('$')
+  if ai_type == 'i' then
+    -- Skip first and last blank lines for `i` textobject
+    local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+    -- Do nothing for buffer with all blanks
+    if first_nonblank == 0 or last_nonblank == 0 then
+      return { from = { line = start_line, col = 1 } }
+    end
+    start_line, end_line = first_nonblank, last_nonblank
+  end
+
+  local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+  return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
+end
+
 local ai = require('mini.ai')
 local mini_ai_opts = {
   n_lines = 500,
   custom_textobjects = {
-    c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }), -- class
+    c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }),
     d = { '%f[%d]%d+' }, -- digits
     e = { -- Word with case
       { '%u[%l%d]+%f[^%l%d]', '%f[%S][%l%d]+%f[^%l%d]', '%f[%P][%l%d]+%f[^%l%d]', '^[%l%d]+%f[^%l%d]' },
       '^().*()$',
     },
     f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }), -- function
-    g = LazyVim.mini.ai_buffer, -- buffer
+    g = ai_buffer,
     o = ai.gen_spec.treesitter({ -- code block
       a = { '@block.outer', '@conditional.outer', '@loop.outer' },
       i = { '@block.inner', '@conditional.inner', '@loop.inner' },
@@ -18,5 +35,6 @@ local mini_ai_opts = {
     U = ai.gen_spec.function_call({ name_pattern = '[%w_]' }), -- without dot in function name
   },
 }
+
 require('mini.ai').setup(mini_ai_opts)
 require('lazy.spec.mini.ai_whichkey').register(mini_ai_opts)
--- lua/lazy/spec/mini/ai_whichkey.lua
+++ lua/lazy/spec/mini/ai_whichkey.lua
@@ -1,23 +1,6 @@
 ---@class lazyvim.util.mini
 local M = {}
 
--- taken from MiniExtra.gen_ai_spec.buffer
-function M.ai_buffer(ai_type)
-  local start_line, end_line = 1, vim.fn.line('$')
-  if ai_type == 'i' then
-    -- Skip first and last blank lines for `i` textobject
-    local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
-    -- Do nothing for buffer with all blanks
-    if first_nonblank == 0 or last_nonblank == 0 then
-      return { from = { line = start_line, col = 1 } }
-    end
-    start_line, end_line = first_nonblank, last_nonblank
-  end
-
-  local to_col = math.max(vim.fn.getline(end_line):len(), 1)
-  return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
-end
-
 -- register all text objects with which-key
 ---@param opts table
 function M.register(opts)
