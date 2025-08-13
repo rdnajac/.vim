25b1a332b57d37fb5395c0e4a73fe0fbdd61c86a
Ryan D. Najac <ryan.najac@columbia.edu>
2025-08-13 14:43:02 -0400

    override vim.notify

--- init.lua
+++ init.lua
@@ -1,11 +1,30 @@
 -- init.lua
 -- see `$VIMRUNTIME/example_init.lua`
+
+---@param msg string Content of the notification to show to the user.
+---@param level integer|nil One of the values from |vim.log.levels|.
+---@param opts table|nil Optional parameters. Unused by default.
+---@diagnostic disable-next-line
+function vim.notify(msg, level, opts)
+  local chunks = {
+    {
+      msg,
+      level == vim.log.levels.DEBUG and 'Statement'
+        or level == vim.log.levels.INFO and 'MoreMsg'
+        or level == vim.log.levels.WARN and 'WarningMsg'
+        or nil,
+    },
+  }
+  vim.api.nvim_echo(chunks, true, { err = level == vim.log.levels.ERROR })
+end
+
 vim.g.plug_home = vim.fs.joinpath(vim.fn.stdpath('data'), 'site', 'pack', 'core', 'opt')
 
 vim.cmd('runtime vimrc')
 
 vim.loader.enable()
 
+-- try `:options`
 vim.o.cmdheight = 0
 vim.o.pumblend = 0
 vim.o.winborder = 'rounded'
@@ -31,9 +50,9 @@ Plug('nvim.colorscheme')
 Plug('nvim.mini')
 Plug('nvim.lsp')
 Plug('nvim.treesitter')
+require('chromatophore')
 
 _G.icons = require('nvim.icons')
 _G.plugins = require('plugins')
 
-require('chromatophore')
 require('nvim.config')
