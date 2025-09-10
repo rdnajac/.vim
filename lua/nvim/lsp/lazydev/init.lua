93879e4557afcc8e5e82051e7de00fd832b5f4aa
Ryan D. Najac <ryan.najac@columbia.edu>
2025-09-09 20:57:50 -0400

    rm file

--- lua/nvim/lsp/lazydev/init.lua
+++ lua/nvim/lsp/lazydev/init.lua
@@ -30,7 +30,7 @@ end, {
 
 vim.schedule(function()
   require('nvim.lsp.lazydev.buf').setup()
-  require('nvim.lsp.lazydev.lspconfig').setup()
+  -- M.lspconfig()
 end)
 
 --- Checks if the current buffer is in a workspace:
@@ -43,6 +43,38 @@ function M.find_workspace(buf)
   local Workspace = require('nvim.lsp.lazydev.workspace')
   local ws = Workspace.find({ path = fname })
   return ws and ws.root or nil
+end
+
+function M.lspconfig()
+  local ok, Manager = pcall(require, 'lspconfig.manager')
+  if not ok then
+    return
+  end
+
+  local try_add = Manager.try_add
+
+  --- @param buf integer
+  --- @param project_root? string
+  function Manager:try_add(buf, project_root)
+    local is_supported = false
+    for _, ids in pairs(self._clients) do
+      for _, client_id in ipairs(ids) do
+        local client = vim.lsp.get_client_by_id(client_id)
+        if client and client.name == 'luals' then
+          is_supported = true
+          break
+        end
+      end
+    end
+    -- TODO: should this be moved?
+    if is_supported and not project_root then
+      local root = require('nvim.lsp.lazydev').find_workspace(buf)
+      if root then
+        return self:add(root, false, buf)
+      end
+    end
+    return try_add(self, buf, project_root)
+  end
 end
 
 return M
