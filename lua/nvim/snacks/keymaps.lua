97bfc7a5d54407c3b7fde4bbee93b935c151e412
Ryan D. Najac <ryan.najac@columbia.edu>
2025-09-16 11:51:03 -0400

    mv opt plugin files

--- lua/nvim/snacks/keymaps.lua
+++ lua/nvim/snacks/keymaps.lua
@@ -50,12 +50,13 @@ Snacks.toggle.indent():map('<leader>ug')
 Snacks.toggle.scroll():map('<leader>us')
 Snacks.toggle.words():map('<leader>uw')
 Snacks.toggle.zoom():map('<leader>uZ')
-Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-  Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-  Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-  Snacks.toggle.line_number():map("<leader>ul")
-  -- Snacks.toggle.inlay_hints():map("<leader>uh")
+Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
+Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
+Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>uL')
+Snacks.toggle.line_number():map('<leader>ul')
+-- Snacks.toggle.inlay_hints():map('<leader>uh') -- XXX: in lsp.lua
 
+-- stylua: ignore
 local keys = {
   -- Top Pickers & Explorer
   { '<leader><space>', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
@@ -123,6 +124,7 @@ local keys = {
   -- { 'gy',         function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
   -- { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
   -- { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
+  -- TODO: make this a func to pull up any doc with pre-defined win opts
   {
     '<leader>N',
     desc = 'Neovim News',
@@ -131,6 +133,7 @@ local keys = {
         file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
         width = 0.6,
         height = 0.6,
+        border = 'double',
         wo = {
           spell = false,
           wrap = false,
