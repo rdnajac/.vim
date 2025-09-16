62686c369c7fbd4f93d02a4539a25eec99d9fcf2
Ryan D. Najac <ryan.najac@columbia.edu>
2025-09-16 14:24:51 -0400

    mini.icons fix
    oil already prefers mini.icons so no need to do the
    package.loaded['nvim-web-devicons' hack anymore

--- lua/nvim/mini/init.lua
+++ lua/nvim/mini/init.lua
@@ -5,5 +5,8 @@ return {
     require('nvim.util').for_each_module(function(minimod)
       require(minimod)
     end, 'nvim/mini')
+    -- just call these manually
+    require('mini.icons').setup(nv.mini.icons)
+    require('mini.align').setup({})
   end,
 }
