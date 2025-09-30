057e91964cd437d45ddb334ae8fc8f74a26ea882
Ryan D. Najac <ryan.najac@columbia.edu>
2025-09-30 14:57:04 -0400

    fmt

--- lua/nvim/folke.lua
+++ lua/nvim/folke.lua
@@ -45,22 +45,22 @@ return {
     opts = {
       lsp = {
         override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
         },
       },
       routes = {
         {
           filter = {
-            event = "msg_show",
+            event = 'msg_show',
             any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
+              { find = '%d+L, %d+B' },
+              { find = '; after #%d+' },
+              { find = '; before #%d+' },
             },
           },
-          view = "mini",
+          view = 'mini',
         },
       },
       presets = {
@@ -68,7 +68,7 @@ return {
         command_palette = true,
         long_message_to_split = true,
       },
-    }
+    },
   },
   {
     'folke/flash.nvim',
