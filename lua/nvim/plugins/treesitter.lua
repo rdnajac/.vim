a84b9050e2b5d68b40698e120ccc41435f825942
Ryan D. Najac <ryan.najac@columbia.edu>
2025-10-07 20:31:54 -0400

    WIP TS

--- lua/nvim/plugins/treesitter.lua
+++ lua/nvim/plugins/treesitter.lua
@@ -13,89 +13,89 @@ local autostart = function(ft, override)
   })
 end
 
+local autostart_filetypes =
+  { 'markdown', 'python', 'yaml', 'json', 'html', 'css', 'javascript', 'typescript', 'toml' }
+
 local M = {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      print('Configuring nvim-treesitter')
-      autostart({ 'markdown', 'python' })
-      autostart({ 'sh', 'zsh' }, 'bash')
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    autostart(autostart_filetypes)
+    autostart({ 'sh', 'zsh' }, 'bash')
 
-      vim.keymap.set('n', '<C-Space>', function()
-        require('nvim.util.treesitter.selection').start()
-      end, { desc = 'TS Select Node', silent = true })
+    vim.keymap.set('n', '<C-Space>', function()
+      nv.treesitter.selection.start()
+    end, { desc = 'TS Select Node', silent = true })
 
-      vim.keymap.set('x', '<C-Space>', function()
-        require('nvim.util.treesitter.selection').increment()
-      end, { desc = 'TS Expand Selection', silent = true })
+    vim.keymap.set('x', '<C-Space>', function()
+      nv.treesitter.selection.increment()
+    end, { desc = 'TS Expand Selection', silent = true })
 
-      vim.keymap.set('x', '<BS>', function()
-        require('nvim.util.treesitter.selection').decrement()
-      end, { desc = 'TS Shrink Selection', silent = true })
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    enabled = 'false',
-    opts = {
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        -- LazyVim extention to create buffer-local keymaps
-        keys = {
-          goto_next_start = {
-            [']f'] = '@function.outer',
-            [']c'] = '@class.outer',
-            [']a'] = '@parameter.inner',
-          },
-          goto_next_end = {
-            [']F'] = '@function.outer',
-            [']C'] = '@class.outer',
-            [']A'] = '@parameter.inner',
-          },
-          goto_previous_start = {
-            ['[f'] = '@function.outer',
-            ['[c'] = '@class.outer',
-            ['[a'] = '@parameter.inner',
-          },
-          goto_previous_end = {
-            ['[F'] = '@function.outer',
-            ['[C'] = '@class.outer',
-            ['[A'] = '@parameter.inner',
-          },
-        },
-      },
-    },
-    after = function(_, opts)
-      vim.api.nvim_create_autocmd('FileType', {
-        group = vim.api.nvim_create_augroup('lazyvim_treesitter_textobjects', { clear = true }),
-        callback = function(ev)
-          ---@type table<string, table<string, string>>
-          -- FIXME: move the moves from opts here
-          local moves = vim.tbl_get(opts, 'move', 'keys') or {}
-
-          for method, keymaps in pairs(moves) do
-            for key, query in pairs(keymaps) do
-              local desc = query:gsub('@', ''):gsub('%..*', '')
-              desc = desc:sub(1, 1):upper() .. desc:sub(2)
-              desc = (key:sub(1, 1) == '[' and 'Prev ' or 'Next ') .. desc
-              desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and ' End' or ' Start')
-              if not (vim.wo.diff and key:find('[cC]')) then
-                vim.keymap.set({ 'n', 'x', 'o' }, key, function()
-                  require('nvim-treesitter-textobjects.move')[method](query, 'textobjects')
-                end, {
-                  buffer = ev.buf,
-                  desc = desc,
-                  silent = true,
-                })
-              end
-            end
-          end
-        end,
-      })
-    end,
-  },
+    vim.keymap.set('x', '<BS>', function()
+      nv.treesitter.selection.decrement()
+    end, { desc = 'TS Shrink Selection', silent = true })
+  end,
+  -- {
+  --   'nvim-treesitter/nvim-treesitter-textobjects',
+  --   enabled = 'false',
+  --   opts = {
+  --     move = {
+  --       enable = true,
+  --       set_jumps = true, -- whether to set jumps in the jumplist
+  --       -- LazyVim extention to create buffer-local keymaps
+  --       keys = {
+  --         goto_next_start = {
+  --           [']f'] = '@function.outer',
+  --           [']c'] = '@class.outer',
+  --           [']a'] = '@parameter.inner',
+  --         },
+  --         goto_next_end = {
+  --           [']F'] = '@function.outer',
+  --           [']C'] = '@class.outer',
+  --           [']A'] = '@parameter.inner',
+  --         },
+  --         goto_previous_start = {
+  --           ['[f'] = '@function.outer',
+  --           ['[c'] = '@class.outer',
+  --           ['[a'] = '@parameter.inner',
+  --         },
+  --         goto_previous_end = {
+  --           ['[F'] = '@function.outer',
+  --           ['[C'] = '@class.outer',
+  --           ['[A'] = '@parameter.inner',
+  --         },
+  --       },
+  --     },
+  --   },
+  --   after = function(_, opts)
+  --     vim.api.nvim_create_autocmd('FileType', {
+  --       group = vim.api.nvim_create_augroup('lazyvim_treesitter_textobjects', { clear = true }),
+  --       callback = function(ev)
+  --         ---@type table<string, table<string, string>>
+  --         -- FIXME: move the moves from opts here
+  --         local moves = vim.tbl_get(opts, 'move', 'keys') or {}
+  --
+  --         for method, keymaps in pairs(moves) do
+  --           for key, query in pairs(keymaps) do
+  --             local desc = query:gsub('@', ''):gsub('%..*', '')
+  --             desc = desc:sub(1, 1):upper() .. desc:sub(2)
+  --             desc = (key:sub(1, 1) == '[' and 'Prev ' or 'Next ') .. desc
+  --             desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and ' End' or ' Start')
+  --             if not (vim.wo.diff and key:find('[cC]')) then
+  --               vim.keymap.set({ 'n', 'x', 'o' }, key, function()
+  --                 require('nvim-treesitter-textobjects.move')[method](query, 'textobjects')
+  --               end, {
+  --                 buffer = ev.buf,
+  --                 desc = desc,
+  --                 silent = true,
+  --               })
+  --             end
+  --           end
+  --         end
+  --       end,
+  --     })
+  --   end,
+  -- },
 }
 
 -- TODO:  report language
