c655f1469752e0cf655eb151eb332f24754b14ea
Ryan D. Najac <ryan.najac@columbia.edu>
2025-10-13 00:36:47 -0400

    add default bigfile setup

--- lua/nvim/snacks/opts.lua
+++ lua/nvim/snacks/opts.lua
@@ -1,7 +1,22 @@
 ---@module "snacks"
 ---@type snacks.Config
 return {
-  bigfile = { enabled = true },
+  -- bigfile = { enabled = true },
+bigfile = {
+  ---@param ctx {buf: number, ft:string}
+  setup = function(ctx)
+    if vim.fn.exists(":NoMatchParen") ~= 0 then
+      vim.cmd([[NoMatchParen]])
+    end
+    Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
+    vim.b.minianimate_disable = true
+    vim.schedule(function()
+      if vim.api.nvim_buf_is_valid(ctx.buf) then
+        vim.bo[ctx.buf].syntax = ctx.ft
+      end
+    end)
+  end,
+},
   dashboard = require('nvim.snacks.dashboard'),
   explorer = { replace_netrw = false }, -- using `oil` instead
   image = { enabled = true },
