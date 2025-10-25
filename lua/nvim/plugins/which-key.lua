d3f97a0bf4e4bb47948305c0e6a7481811efc82f
Ryan D. Najac <ryan.najac@columbia.edu>
2025-10-25 00:14:21 -0400

    prepare to extend

--- lua/nvim/plugins/which-key.lua
+++ lua/nvim/plugins/which-key.lua
@@ -1,3 +1,27 @@
+local spec = {
+  {
+    mode = { 'n', 'v' },
+    -- TODO: add each bracket mapping manually
+    { '[', group = 'prev' },
+    { ']', group = 'next' },
+    { 'g', group = 'goto' },
+    { 'z', group = 'fold' },
+  },
+  {
+    mode = { 'n' },
+    { 'co', group = 'comment below' },
+    { 'cO', group = 'comment above' },
+    { '<localleader>l', group = 'vimtex' },
+    { '<localleader>r', group = 'R', icon = { icon = ' ', color = 'blue' } },
+
+    -- descriptions
+    { 'gx', desc = 'Open with system app' },
+    { 'ga', mode = 'x', desc = 'Align' },
+    { 'gA', mode = 'x', desc = 'Align (preview)' },
+  },
+  { hidden = true, { 'g~' }, { 'g#' }, { 'g*' } },
+}
+
 return {
   'folke/which-key.nvim',
   --- @class wk.Opts
@@ -6,32 +30,10 @@ return {
     preset = 'helix',
     show_help = false,
     sort = { 'order', 'alphanum', 'case', 'mod' },
-    spec = {
-      {
-        mode = { 'n', 'v' },
-        -- TODO: add each bracket mapping manually
-        { '[', group = 'prev' },
-        { ']', group = 'next' },
-        { 'g', group = 'goto' },
-        { 'z', group = 'fold' },
-      },
-      {
-        mode = { 'n' },
-        { 'co', group = 'comment below' },
-        { 'cO', group = 'comment above' },
-        { '<localleader>l', group = 'vimtex' },
-        { '<localleader>r', group = 'R', icon = { icon = ' ', color = 'blue' } },
-
-        -- descriptions
-        { 'gx', desc = 'Open with system app' },
-        { 'ga', mode = 'x', desc = 'Align' },
-        { 'gA', mode = 'x', desc = 'Align (preview)' },
-      },
-      { hidden = true, { 'g~' }, { 'g#' }, { 'g*' } },
-    },
+    spec = spec,
   },
   after = function()
-    -- HACK: global keys
+    -- HACK: global key registration
     require('which-key').add(nv.plug.get_keys())
     local registers = '*+"-:.%/#=_0123456789qZ'
     require('which-key.plugins.registers').registers = registers
