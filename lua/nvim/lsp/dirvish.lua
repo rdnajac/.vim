7d37f1d52b367588e929d2c231c6356f377017b9
Ryan D. Najac <ryan.najac@columbia.edu>
2025-11-16 02:32:31 -0500

    mv file

--- lua/nvim/lsp/dirvish.lua
+++ lua/nvim/lsp/dirvish.lua
@@ -213,24 +213,4 @@ M.status = function()
   return nv.icons.lsp[status]
 end
 
-M.setup = function()
-  vim.g.dirvish_mode = [[:sort ,^.*[\/],]]
-
-  local aug = vim.api.nvim_create_augroup('dirvish.nvim', {})
-
-  vim.api.nvim_create_autocmd('FileType', {
-    group = aug,
-    pattern = 'dirvish',
-    callback = function()
-      -- XXX: dirvish doesn't support highlighting of icons yet
-      require('nvim.icons.fs').render()
-      vim.lsp.buf_attach_client(0, nv.lsp.dirvish.client_id)
-
-      -- BUG: workaround for https://github.com/justinmk/vim-dirvish/issues/257
-      vim.opt_local.listchars = vim.opt.listchars:get()
-      vim.opt_local.listchars:remove('precedes')
-    end,
-  })
-end
-
 return M
