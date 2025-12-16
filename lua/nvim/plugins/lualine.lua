a75cdd27972784cd311318bd088b5c9f75edcc5e
Ryan D. Najac <ryan.najac@columbia.edu>
2025-12-16 18:33:26 -0500

    mv icons

--- lua/nvim/plugins/lualine.lua
+++ lua/nvim/plugins/lualine.lua
@@ -1,107 +1,99 @@
-local nv = _G.nv or require('nvim')
--- TODO: https://github.com/nvim-lualine/lualine.nvim/wiki/Component-snippets
+-- `https://github.com/nvim-lualine/lualine.nvim/wiki/Component-snippets`
 local M = {
   'nvim-lualine/lualine.nvim',
-  opts = function()
-    local opts = {
-      options = {
-        component_separators = { left = '', right = '' },
-        -- section_separators = nv.icons.sep.section.rounded,
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-	  -- FIXME: 
-          statusline = { pattern = 'snacks_dashboard' },
-          winbar = { 'netrw', 'snacks_dashboard', 'snacks_picker_input' },
-          tabline = { 'snacks_dashboard' },
-        },
-        ignore_focus = {
-          -- 'man',
-          -- 'help',
-        },
-        -- theme = {
-        --   normal = { a = 'Chromatophore_a', b = 'Chromatophore_b', c = 'Chromatophore_c' },
-        -- },
-        -- use_mode_colors = false,
+  opts = {
+    options = {
+      component_separators = { left = '', right = '' },
+      -- section_separators = nv.icons.sep.section.rounded,
+      section_separators = { left = '', right = '' },
+      disabled_filetypes = {
+        statusline = { pattern = 'snacks_dashboard' },
+        winbar = { 'netrw', 'snacks_dashboard', 'snacks_picker_input' },
+        tabline = { 'snacks_dashboard' },
       },
+      ignore_focus = {
+        -- 'man',
+        -- 'help',
+      },
+      -- theme = {
+      --   normal = { a = 'Chromatophore_a', b = 'Chromatophore_b', c = 'Chromatophore_c' },
+      -- },
+      -- use_mode_colors = false,
+    },
+    extensions = { 'man' },
+  },
+}
 
-      sections = {
-        lualine_a = {
-          {
-            function()
-              return nv.path.relative_parts()[1]
-            end,
-          },
-        },
-        lualine_b = { nv.path.relative_parts()[2] },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { Snacks.profiler.status() },
-      },
+M.opts.sections = {
+  lualine_a = {
+    {
+      function()
+        return nv.path.relative_parts()[1]
+      end,
+    },
+  },
+  lualine_b = { nv.path.relative_parts()[2] },
+  lualine_c = {},
+  lualine_x = {},
+  lualine_y = {},
+  lualine_z = { Snacks and Snacks.profiler.status() or nil },
+}
 
-      inactive_sections = {},
+M.opts.inactive_sections = {}
 
-      tabline = {
-        lualine_a = {
-          {
-            function()
-              return vim.fn.fnamemodify(Snacks.git.get_root(), ':p:~')
-            end,
-            cond = function()
-              return vim.bo.filetype ~= 'snacks_dashboard'
-            end,
-          },
-        },
-        lualine_b = {
-          {
-            'tabs',
-            -- mode = 0,
-            use_mode_colors = true,
-            path = 0,
-            -- tabs_color = { active = nil, inactive = nil },
-            show_modified_status = true,
-            symbols = { modified = ',+' },
-            cond = function()
-              return vim.bo.filetype ~= 'snacks_dashboard'
-            end,
-          },
-        },
-        lualine_c = {
-          { 'diff', separator = '', cond = function() return vim.bo.filetype ~= 'snacks_dashboard' end },
-          { 'diagnostics', separator = '', cond = function() return vim.bo.filetype ~= 'snacks_dashboard' end },
-        },
-      },
+M.tabline = {
+  lualine_a = {
+    {
+      function()
+        return vim.fn.fnamemodify(Snacks.git.get_root() or '', ':p:~')
+      end,
+    },
+  },
+  lualine_b = {
+    {
+      'tabs',
+      -- mode = 0,
+      use_mode_colors = true,
+      path = 0,
+      -- tabs_color = { active = nil, inactive = nil },
+      show_modified_status = true,
+      symbols = { modified = ',+' },
+    },
+  },
+  lualine_c = {
+    {
+      'diff',
+      separator = '',
+    },
+    {
+      'diagnostics',
+      separator = '',
+    },
+  },
+}
 
-      winbar = {
-        lualine_a = { nv.winbar.a },
-        lualine_b = {
-          {
-            function()
-              if vim.bo.buftype == 'terminal' then
-                return "%{% &buftype == 'terminal' ? ' %{&channel}' : '' %}"
-              end
-              return "%{% &buflisted ? '%n' : '󱪟 ' %}"
-                .. "%{% &bufhidden == '' ? '' : '󰘓 ' %}"
-            end,
-          },
-          -- nv.treesitter.status,
-          -- nv.lsp.status,
-          -- nv.blink.status,
-          -- { require('nvim.plugins.r').status },
-        },
-      },
-      inactive_winbar = {
-        lualine_a = { [[%{%v:lua.nv.icons.filetype()%}]] },
-        lualine_b = { '%t' },
-        -- lualine_c = { M.winbar.b },
-      },
-      extensions = vim.list_extend({ 'man' }, nv.winbar.lualine_extensions),
-    }
-    -- local man = require('lualine/extensions/man')
-    -- man.winbar = man.sections
-    -- vim.list_extend(opts.extensions, man)
-    return opts
-  end,
+M.winbar = {
+  lualine_a = { nv.status.buffer },
+  lualine_b = {
+    {
+      function()
+        if vim.bo.buftype == 'terminal' then
+          return "%{% &buftype == 'terminal' ? ' %{&channel}' : '' %}"
+        end
+        return "%{% &buflisted ? '%n' : '󱪟 ' %}" .. "%{% &bufhidden == '' ? '' : '󰘓 ' %}"
+      end,
+    },
+    -- nv.treesitter.status,
+    -- nv.lsp.status,
+    -- nv.blink.status,
+    -- { require('nvim.plugins.r').status },
+  },
+}
+
+M.inactive_winbar = {
+  lualine_a = { [[%{%v:lua.nv.icons.filetype()%}]] },
+  lualine_b = { '%t' },
+  -- lualine_c = { M.winbar.b },
 }
 
 return M
