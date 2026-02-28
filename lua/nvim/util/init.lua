a8a3f5a66b420db0fdb55e0ca170cde678e9bfc2
Ryan D. Najac <ryan.najac@columbia.edu>
2026-02-28 10:55:53 -0500

    mv fn

--- lua/nvim/util/init.lua
+++ lua/nvim/util/init.lua
@@ -15,31 +15,6 @@ M.is_nonempty_list = function(v) return vim.islist(v) and #v > 0 end
 -- fn wrappers
 M.synname = function(row, col) return fn.synIDattr(fn.synID(row, col, 1), 'name') end
 
---- Normalize position arguments into a vim.Pos, falling back to cursor.
----@param ... any
----  - no args:       cursor position
----  - (row, col):    0-based integers → vim.pos(row, col)
----  - {row, col}:    extmark tuple (0-based) → vim.pos.extmark(arg)
----  - vim.Pos:       returned as-is
----@return vim.Pos
-function M.pos(...)
-  local nargs = select('#', ...)
-  if nargs == 0 then
-    return vim.pos.cursor(api.nvim_win_get_cursor(0))
-  elseif nargs == 2 then
-    return vim.pos(...)
-  elseif nargs == 1 then
-    local arg = select(1, ...)
-    if type(arg) == 'table' then
-      if arg.row then
-        return arg
-      end -- already a vim.Pos
-      return vim.pos.extmark(arg) -- {row, col} extmark tuple
-    end
-  end
-  error('pos: invalid arguments')
-end
-
 -- api wrappers
 M.get_buf_lines = function(bufnr)
   bufnr = bufnr or api.nvim_get_current_buf()
