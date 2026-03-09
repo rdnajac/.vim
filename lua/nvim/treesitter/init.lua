14f2a9047d9809596934e93e9ffe32c2869fd086
Ryan D. Najac <ryan.najac@columbia.edu>
2026-03-09 14:40:37 -0400

    use new native ts incremental selection

--- lua/nvim/treesitter/init.lua
+++ lua/nvim/treesitter/init.lua
@@ -1,85 +1,67 @@
-local M = {}
-
-M.after = function()
-  -- delay loading this table until after init
-  M.parsers = require('nvim.treesitter.parsers')
-
-  local aug = vim.api.nvim_create_augroup('treesitter', {})
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = M.parsers.to_autostart,
-    group = aug,
-    callback = function(ev) vim.treesitter.start(ev.buf) end,
-    desc = 'Automatically start tree-sitter',
-  })
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'markdown', 'r', 'rmd', 'quarto' },
-    group = aug,
-    command = 'setlocal foldmethod=expr foldexpr=v:lua.vim.treesitter.foldexpr()',
-    desc = 'Use treesitter folding for select filetypes',
-  })
-end
-
--- TODO: remove when native ts selection is merged
-M.selection = require('nvim.treesitter.selection')
-
-M.specs = {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = function() vim.cmd('TSUpdate') end,
-    -- TODO: Don't re-install up-to-date parsers
-    -- build = function() vim.cmd('TSUpdate | TSInstall! ' .. table.concat(M.parsers.to_install, ' ')) end,
-    keys = {
-      { 'n', '<C-Space>', M.selection.start, { desc = 'Start ts selection' } },
-      { 'x', '<C-Space>', M.selection.increment, { desc = 'Increment ts selection' } },
-      { 'x', '<BS>', M.selection.decrement, { desc = 'Decrement ts selection' } },
-    },
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    enabled = false,
-    toggles = {
-      ['<leader>ux'] = {
-        name = 'Treesitter Context',
-        get = function() return require('treesitter-context').enabled() end,
-        set = function() require('treesitter-context').toggle() end,
-      },
-    },
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    enabled = false,
-    opts = {
-      move = { set_jumps = true },
-      select = {
-        lookahead = true,
-        selection_modes = {
-          ['@parameter.outer'] = 'v', -- charwise
-          ['@function.outer'] = 'V', -- linewise
-          ['@class.outer'] = '<c-v>', -- blockwise
+local M = {
+  parsers = require('nvim.treesitter.parsers'),
+  specs = {
+    {
+      'nvim-treesitter/nvim-treesitter-context',
+      enabled = false,
+      toggles = {
+        ['<leader>ux'] = {
+          name = 'Treesitter Context',
+          get = function() return require('treesitter-context').enabled() end,
+          set = function() require('treesitter-context').toggle() end,
         },
-        include_surrounding_whitespace = false,
       },
     },
-    keys = function()
-      -- You can also use captures from other query groups like `locals.scm`
-      -- vim.keymap.set({ 'x', 'o' }, 'as', function()
-      -- require('nvim-treesitter-textobjects.select').select_textobject('@local.scope', 'locals')
-      -- end)
-      local function select_textobject(textobject)
-        return require('nvim-treesitter-textobjects.select').select_textobject(
-          textobject,
-          'textobjects'
-        )
-      end
-      return {
-        { { 'x', 'o' }, 'af', function() select_textobject('@function.outer') end },
-        { { 'x', 'o' }, 'if', function() select_textobject('@function.inner') end },
-        { { 'x', 'o' }, 'ac', function() select_textobject('@class.outer') end },
-        { { 'x', 'o' }, 'ic', function() select_textobject('@class.inner') end },
-      }
-    end,
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      enabled = false,
+      opts = {
+        move = { set_jumps = true },
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V', -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = false,
+        },
+      },
+      keys = function()
+        -- You can also use captures from other query groups like `locals.scm`
+        -- vim.keymap.set({ 'x', 'o' }, 'as', function()
+        -- require('nvim-treesitter-textobjects.select').select_textobject('@local.scope', 'locals')
+        -- end)
+        local function select_textobject(textobject)
+          return require('nvim-treesitter-textobjects.select').select_textobject(
+            textobject,
+            'textobjects'
+          )
+        end
+        return {
+          { { 'x', 'o' }, 'af', function() select_textobject('@function.outer') end },
+          { { 'x', 'o' }, 'if', function() select_textobject('@function.inner') end },
+          { { 'x', 'o' }, 'ac', function() select_textobject('@class.outer') end },
+          { { 'x', 'o' }, 'ic', function() select_textobject('@class.inner') end },
+        }
+      end,
+    },
   },
 }
+
+local aug = vim.api.nvim_create_augroup('treesitter', {})
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = M.parsers.to_autostart,
+  group = aug,
+  callback = function(ev) vim.treesitter.start(ev.buf) end,
+  desc = 'Automatically start tree-sitter',
+})
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'r', 'rmd', 'quarto' },
+  group = aug,
+  command = 'setlocal foldmethod=expr foldexpr=v:lua.vim.treesitter.foldexpr()',
+  desc = 'Use treesitter folding for select filetypes',
+})
 
 M.status = function()
   local ret = {}
@@ -96,13 +78,6 @@ M.status = function()
     end, vim.tbl_keys(queries))
   end
   return table.concat(ret, ' ')
-end
-
-M.install_cli = function()
-  if vim.fn.executable('tree-sitter') == 1 then
-    return
-  end
-  -- TODO: use mason install utility
 end
 
 ---@param node TSNode
