82da04627cf256e260276f0617a6c4a727c1399d
Ryan D. Najac <ryan.najac@columbia.edu>
2026-03-13 13:51:06 -0400

    mv init code

--- init.lua
+++ init.lua
@@ -10,18 +10,18 @@ vim.cmd([[ source ~/.vim/vimrc ]])
 
 require('snacks').setup({
   -- bigfile = { enabled = true },
-  dashboard = require('nvim.snacks.dashboard'),
+  dashboard = require('munchies.dashboard'),
   explorer = { replace_netrw = false },
   image = { enabled = true },
   indent = { indent = { only_current = false, only_scope = true } },
   input = { enabled = true },
-  -- notifier = require('nvim.snacks.notifier'),
+  -- notifier = require('munchies.notifier'),
   quickfile = { enabled = true },
   picker = require('nvim.snacks.picker'),
   -- terminal = { enabled = true },
   scope = { enabled = true },
   scroll = { enabled = true },
-  -- statuscolumn = require('nvim.snacks.statuscolumn'),
+  -- statuscolumn = require('munchies.statuscolumn'),
   styles = { lazygit = { height = 0, width = 0 } },
   toggle = { which_key = false },
   words = { enabled = true },
@@ -31,30 +31,5 @@ _G.dd = Snacks.debug.inspect
 _G.bt = Snacks.debug.backtrace
 _G.p = Snacks.debug.profile
 
---- Defines the structure of modules under the `nvim/` directory
----@class nv.Submodule
----@field specs plug.Spec[]
----@field after? fun():nil
----@field status? fun():string
-
----@type table<string, nv.Submodule>
-_G.nv = {
-  blink = require('nvim.blink'),
-  keys = require('nvim.keys'),
-  lsp = require('nvim.lsp'),
-  treesitter = require('nvim.treesitter'),
-  util = require('nvim.util'),
-}
-
-vim.iter(nv):each(function(_, v)
-  if v.specs then
-    Plug(v.specs)
-  end
-  if vim.is_callable(v.after) then
-    vim.schedule(v.after)
-  end
-end)
-
-nv.ui = require('nvim.ui')
-
+require('nvim')
 -- TODO:  mini root
