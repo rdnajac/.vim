5caed1e8cd0f52bcf112f1a28bf910e6430e74c5
Ryan D. Najac <ryan.najac@columbia.edu>
2026-03-15 20:00:48 -0400

    add fn

--- lua/nvim/util/init.lua
+++ lua/nvim/util/init.lua
@@ -69,4 +69,24 @@ M.yank = function(text)
   print('[yanked] ' .. text)
 end
 
+-- https://github.com/neovim/neovim/discussions/38271#discussion-9630986
+M.get_visual = function()
+  local vis_mode = vim.fn.mode():match("[Vv\22]")
+  if not vis_mode then
+    return
+  end
+  local line_regs = vim.fn.getregionpos(vim.fn.getpos("v"), vim.fn.getpos("."), {
+    type = vis_mode,
+    eol = true,
+    exclusive = false,
+  })
+  local sel_text = {}
+  for _, reg in ipairs(line_regs) do
+    local r1, c1, r2, c2 = reg[1][2], reg[1][3], reg[2][2], reg[2][3]
+    local buf_text = vim.api.nvim_buf_get_text(0, r1 - 1, c1 - 1, r2 - 1, c2, {})
+    vim.list_extend(sel_text, buf_text)
+  end
+  return sel_text, line_regs
+end
+
 return M
