133b9db5927dfc1b7540a435f2308a9eccca9c4b
Ryan D. Najac <ryan.najac@columbia.edu>
2026-03-17 18:00:28 -0400

    unset ai mappings

--- plugin/mini/ai.lua
+++ plugin/mini/ai.lua
@@ -1,6 +1,12 @@
 local ai = require('mini.ai')
 local ex = require('mini.extra')
 local opts = {
+  mappings = {
+    around_next = 'aN',
+    inside_next = 'iN',
+    around_last = 'aL',
+    inside_last = 'iL',
+  },
   n_lines = 500,
   custom_textobjects = {
     -- c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }), -- class
@@ -27,4 +33,12 @@ local opts = {
   },
 }
 
--- ai.setup(opts)
+ai.setup(opts)
+
+-- local map_lsp_selection = function(lhs, desc)
+--   local s = vim.startswith(desc, 'Increase') and 1 or -1
+--   local rhs = function() vim.lsp.buf.selection_range(s * vim.v.count1) end
+--   vim.keymap.set('x', lhs, rhs, { desc = desc })
+-- end
+-- map_lsp_selection('<C-Space>', 'Increase selection')
+-- map_lsp_selection('<BS>', 'Decrease selection')
